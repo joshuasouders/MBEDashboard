3bc7e26a775fd412f03d76f570a284dcbd197a20
Josh Souders <rem87062@gmail.com>
2014-06-13 11:17:26 -0400

    populated county dropdown

--- js/panel.js
+++ js/panel.js
@@ -8,13 +8,21 @@ Panel.prototype.setData = function(data){
 }
 
 Panel.prototype.populateDropdown = function(){
-	var industryTypes = new Array({"SHORT_CODE": this.data.items[0].SHORT_CODE, "SHORT_DESC": this.data.items[0].SHORT_DESC});
+	var industry = new Array({"SHORT_CODE": this.data.items[0].SHORT_CODE, "SHORT_DESC": this.data.items[0].SHORT_DESC});
+	var specialty = new Array({"SHORT_CODE": this.data.items[0].NAICS_CODE, "SHORT_DESC": this.data.items[0].NAICS_DESC});
 
+	//populate industry dropdown
 	for(var i = 0; i < this.data.items.length; i++){
 		var obj = {"SHORT_CODE": this.data.items[i].SHORT_CODE, "SHORT_DESC": this.data.items[i].SHORT_DESC};
-		if(industryTypes[industryTypes.length-1].SHORT_CODE != obj.SHORT_CODE){
-			industryTypes.push(obj);
+		if(industry[industry.length-1].SHORT_CODE != obj.SHORT_CODE){
+			industry.push(obj);
 			$('#industryDropdown').append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'+obj.SHORT_DESC+' ('+obj.SHORT_CODE+')</a></li>');
 		}
+	}
+
+	//populate county dropdown
+	var counties = new Array("Allegany", "Anne Arundel", "Baltimore", "Baltimore City", "Calvert", "Caroline", "Carroll", "Cecil", "Charles", "Dorchester", "Frederick", "Garrett", "Harford", "Howard", "Kent", "Montgomery", "Prince George's", "Queen Anne's", "Somerset", "St. Mary's", "Talbot", "Washington", "Wicomico", "Worcester");
+	for(var i = 0; i < counties.length; i++){
+		$('#countyDropdown').append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'+counties[i]+'</a></li>');
 	}
 }
